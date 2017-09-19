357822581249ef5216cea331a3e581e0a455f485
Ali Alfarej <31965381+aalfarej@users.noreply.github.com>
2017-09-19 14:22:25 -0400

    Add files via upload

---
+++ sentence.py
@@ -0,0 +1,14 @@
+6 * (1 - 2)
+A = "All"
+B = "Work"
+C = "and"
+D = "no"
+E = "play"
+F = "makes"
+G = "Jack"
+H = "a"
+I = "dull"
+J = "boy"
+print (A,B,C,D,E,F,G,H,I,J)
+bruce = 6
+print (bruce + 4)
