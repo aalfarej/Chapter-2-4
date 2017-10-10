564d2a411cc1d18109e1a15811c9842b6ea4f88c
Ali Alfarej <31965381+aalfarej@users.noreply.github.com>
2017-10-10 14:29:56 -0400

    Delete Months .py

--- Months .py
+++
@@ -1,2 +0,0 @@
-for i in ["January", "February", "March", "April", "May", "June", "July", "August", "September", "obctober", "November", "December"]:
-    print("One of the months of the year is "+i)
