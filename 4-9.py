26df42ca7c0e18045a3e71bc4ecdfdaedb4aeb83
Ali Alfarej <31965381+aalfarej@users.noreply.github.com>
2017-10-10 10:29:42 -0400

    Add files via upload

---
+++ 4-9.py
@@ -0,0 +1,11 @@
+import turtle
+wn = turtle.Screen()
+wn.bgcolor("red")
+wn.title("hello, Alex")
+tess = turtle.Turtle()
+tess.pensize(4)
+for i in range(5):
+    tess.forward(100)
+    tess.right(144)
+    
+tess.hideturtle()
