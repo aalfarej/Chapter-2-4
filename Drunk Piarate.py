45e94ba4dfb48b5adcf92af30526462cbfc28e2e
Ali Alfarej <31965381+aalfarej@users.noreply.github.com>
2017-10-10 14:29:05 -0400

    Delete Drunk Piarate.py

--- Drunk Piarate.py
+++
@@ -1,9 +0,0 @@
-import turtle
-wn = turtle.Screen()
-allie = turtle.Turtle()
-turns = [160, -43, 270, -97, -43, 200, -940, 17, -86]
-for i in turns:
-    allie.left(i)
-    allie.forward(100)
-print (allie.heading())
-    
