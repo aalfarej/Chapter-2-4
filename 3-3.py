6e7b5e9421fb2373d2f733e827adbb3f64d1d26e
Ali Alfarej <31965381+aalfarej@users.noreply.github.com>
2017-10-10 14:30:23 -0400

    Add files via upload

---
+++ 3-3.py
@@ -0,0 +1,2 @@
+for i in ["January", "February", "March", "April", "May", "June", "July", "August", "September", "obctober", "November", "December"]:
+    print("One of the months of the year is "+i)
