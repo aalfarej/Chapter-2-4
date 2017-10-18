6b16ca9d8e124eccb70d8134234f378f74ffde55
Ali Alfarej <31965381+aalfarej@users.noreply.github.com>
2017-10-18 14:36:11 -0400

    Add files via upload

---
+++ 3-8.py
@@ -0,0 +1,9 @@
+from turtle import *
+wn = Screen()
+sarah = Turtle()
+
+for i in [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17]:      #repeat four times
+    sarah.forward(50)
+    sarah.left(20)
+
+wn.exitonclick()
